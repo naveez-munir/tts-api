5891dc308ed84ef55da85c72c973bdd5c06b62d3
Naveez Munir <naveezmunir@gmail.com>
2026-01-01 11:00:00 +0500

    chore(payments): register Stripe and payment dependencies

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service.js';
 import { PaymentsController } from './payments.controller.js';
 import { StripeModule } from '../../integrations/stripe/stripe.module.js';
+import { NotificationsModule } from '../../integrations/notifications/notifications.module.js';
+import { BiddingQueueModule } from '../../queue/bidding-queue.module.js';
 
 @Module({
-  imports: [StripeModule],
+  imports: [StripeModule, NotificationsModule, BiddingQueueModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
