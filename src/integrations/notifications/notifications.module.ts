4fc8b1861b70ee503ef39223411fbcc7c27c55f5
Naveez Munir <naveezmunir@gmail.com>
2026-01-11 09:20:00 +0500

    feat(notifications): update notifications module

--- src/integrations/notifications/notifications.module.ts
+++ src/integrations/notifications/notifications.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service.js';
-import { SendGridModule } from '../sendgrid/sendgrid.module.js';
+import { ResendModule } from '../resend/resend.module.js';
 import { TwilioModule } from '../twilio/twilio.module.js';
 import { DatabaseModule } from '../../database/database.module.js';
+import { SystemSettingsModule } from '../../modules/system-settings/system-settings.module.js';
 
 @Module({
-  imports: [SendGridModule, TwilioModule, DatabaseModule],
+  imports: [ResendModule, TwilioModule, DatabaseModule, SystemSettingsModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
