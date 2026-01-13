71ecf8a850be81745495ff5ea5a0952517e25e5f
Naveez Munir <naveezmunir@gmail.com>
2026-01-13 09:00:00 +0500

    feat(stripe): update Stripe module configuration

--- src/integrations/stripe/stripe.module.ts
+++ src/integrations/stripe/stripe.module.ts
@@ -3,9 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { StripeService } from './stripe.service.js';
 import { StripeWebhookController } from './stripe-webhook.controller.js';
 import { DatabaseModule } from '../../database/database.module.js';
+import { JobsModule } from '../../modules/jobs/jobs.module.js';
+import { SystemSettingsModule } from '../../modules/system-settings/system-settings.module.js';
+import { NotificationsModule } from '../notifications/notifications.module.js';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, JobsModule, SystemSettingsModule, NotificationsModule],
   controllers: [StripeWebhookController],
   providers: [StripeService],
   exports: [StripeService],
