07a9fd9afd9dc7f9fe999be9a873ce19f24669b6
Naveez Munir <naveezmunir@gmail.com>
2026-01-14 10:30:00 +0500

    feat(google-maps): update Google Maps module

--- src/integrations/google-maps/google-maps.module.ts
+++ src/integrations/google-maps/google-maps.module.ts
@@ -4,9 +4,10 @@ import { GoogleMapsService } from './google-maps.service.js';
 import { GoogleMapsController } from './google-maps.controller.js';
 import { QuoteService } from './quote.service.js';
 import { DatabaseModule } from '../../database/database.module.js';
+import { SystemSettingsModule } from '../../modules/system-settings/system-settings.module.js';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, SystemSettingsModule],
   controllers: [GoogleMapsController],
   providers: [GoogleMapsService, QuoteService],
   exports: [GoogleMapsService, QuoteService],
