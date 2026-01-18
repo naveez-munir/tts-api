7a722b30dcd8b32648d94ab5c1196988897a4ffa
Naveez Munir <naveezmunir@gmail.com>
2026-01-18 18:50:00 +0500

    feat(bookings): update bookings module

--- src/modules/bookings/bookings.module.ts
+++ src/modules/bookings/bookings.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { BookingsService } from './bookings.service.js';
 import { BookingsController } from './bookings.controller.js';
+import { VehicleCapacityModule } from '../vehicle-capacity/vehicle-capacity.module.js';
+import { SystemSettingsModule } from '../system-settings/system-settings.module.js';
+import { GoogleMapsModule } from '../../integrations/google-maps/google-maps.module.js';
+import { StripeModule } from '../../integrations/stripe/stripe.module.js';
+import { NotificationsModule } from '../../integrations/notifications/notifications.module.js';
 
 @Module({
+  imports: [
+    VehicleCapacityModule,
+    SystemSettingsModule,
+    GoogleMapsModule,
+    StripeModule,
+    NotificationsModule,
+  ],
   controllers: [BookingsController],
   providers: [BookingsService],
   exports: [BookingsService],
