0da218c0db629eab8dcc35cf7b6f506c9bdff284
Naveez Munir <naveezmunir@gmail.com>
2026-01-20 14:25:00 +0500

    feat(payments): update payments module

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service.js';
 import { PaymentsController } from './payments.controller.js';
 import { StripeModule } from '../../integrations/stripe/stripe.module.js';
-import { NotificationsModule } from '../../integrations/notifications/notifications.module.js';
-import { BiddingQueueModule } from '../../queue/bidding-queue.module.js';
 
 @Module({
-  imports: [StripeModule, NotificationsModule, BiddingQueueModule],
+  imports: [StripeModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
   exports: [PaymentsService],
