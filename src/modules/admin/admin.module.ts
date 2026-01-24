9394ae5c04c456209b4c5b54b702af403e88add2
Naveez Munir <naveezmunir@gmail.com>
2026-01-24 09:00:00 +0500

    feat(admin): update admin module

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller.js';
 import { AdminService } from './admin.service.js';
 import { StripeModule } from '../../integrations/stripe/stripe.module.js';
+import { SystemSettingsModule } from '../system-settings/system-settings.module.js';
+import { BiddingQueueModule } from '../../queue/bidding-queue.module.js';
+import { S3Module } from '../../integrations/s3/s3.module.js';
 
 @Module({
-  imports: [StripeModule],
+  imports: [StripeModule, SystemSettingsModule, BiddingQueueModule, S3Module],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
